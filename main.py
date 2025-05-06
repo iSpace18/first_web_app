c94d2da62d0ece513854e132963373a269d876d5
ispace18 <steamdavid2007@gmail.com>
2025-05-06 23:00:47 +0300

    123

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import requests
 from flask import Flask, render_template, request
 
-app = Flask(__name__) # Или app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='.')  # template_folder='.' указывает на текущую директорию
 
 @app.route("/", methods=['GET', 'POST'])
 def web():
-        price = None
-        error_message = None
+    price = None
+    error_message = None
 
-        if request.method == 'POST':
-            pair = request.form['pair'].upper()
-            url = f'https://api.binance.com/api/v3/ticker/price?symbol={pair}'
-            try:
-                response = requests.get(url)
-                data = response.json()
+    if request.method == 'POST':
+        pair = request.form['pair'].upper()
+        url = f'https://api.binance.com/api/v3/ticker/price?symbol={pair}'
+        try:
+            response = requests.get(url)
+            data = response.json()
 
-                if 'price' in data:
-                    price = data['price']
-                else:
-                    error_message = "Не удалось получить цену для указанной пары. Проверьте правильность ввода."
-            except requests.exceptions.RequestException as e:
-                error_message = f"Ошибка подключения к Binance API: {e}"
+            if 'price' in data:
+                price = data['price']
+            else:
+                error_message = "Не удалось получить цену для указанной пары. Проверьте правильность ввода."
+        except requests.exceptions.RequestException as e:
+            error_message = f"Ошибка подключения к Binance API: {e}"
 
-        return render_template('index.html', price=price, error_message=error_message)
+    return render_template('index.html', price=price, error_message=error_message)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port='80')
